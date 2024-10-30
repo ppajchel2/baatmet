2f47f1438bd57a976c936b45db57c9ac96424662
Piotr Pajchel <piotrp@met.no>
2024-10-30 09:44:58 +0100

    co1

--- bot.py
+++ bot.py
@@ -25,15 +25,12 @@ class Bot:
         self.team = "TeamName"  # This is your team name
         # This is the course that the ship has to follow
         self.course = [
-            Checkpoint(latitude=43.797109, longitude=-11.264905, radius=50),
-            Checkpoint(longitude=-29.908577, latitude=17.999811, radius=50),
-            Checkpoint(latitude=-11.441808, longitude=-29.660252, radius=50),
-            Checkpoint(longitude=-63.240264, latitude=-61.025125, radius=50),
+            Checkpoint(latitude=20.03, longitude=-67.31, radius=50),
+            Checkpoint(latitude=18.04, longitude=-67.85, radius=50),
+            Checkpoint(latitude=13.03, longitude=-80.97, radius=50),
+            Checkpoint(latitude=08.51, longitude=-79.42, radius=50),
             Checkpoint(latitude=2.806318, longitude=-168.943864, radius=1990.0),
-            Checkpoint(latitude=-62.052286, longitude=169.214572, radius=50.0),
             Checkpoint(latitude=-15.668984, longitude=77.674694, radius=1190.0),
-            Checkpoint(latitude=-39.438937, longitude=19.836265, radius=50.0),
-            Checkpoint(latitude=14.881699, longitude=-21.024326, radius=50.0),
             Checkpoint(latitude=44.076538, longitude=-18.292936, radius=50.0),
             Checkpoint(
                 latitude=config.start.latitude,
